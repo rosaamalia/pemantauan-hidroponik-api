969157ae522aef675b97411484d086aff3261851
rosaamalia <amaliarosaaa15@gmail.com>
2023-05-26 23:30:09 +0700

    menambahkan signal untuk membuat data kebun_disematkan setiap ada data akun baru

--- akun/signals.py
+++ akun/signals.py
@@ -1,7 +1,8 @@
-from django.db.models.signals import pre_save
+from django.db.models.signals import pre_save, post_save
 from django.dispatch import receiver
 from django.utils import timezone
-from .models import KodeVerifikasi
+from .models import KodeVerifikasi, Akun
+from kebun.models import KebunDisematkan
 
 @receiver(pre_save, sender=KodeVerifikasi)
 def set_waktu_kadaluarsa(sender, instance, **kwargs):
@@ -12,3 +13,8 @@ def set_waktu_kadaluarsa(sender, instance, **kwargs):
         # instance sudah pernah disimpan sebelumnya, berarti ini adalah update
         # waktu_kadaluarsa tidak perlu diubah karena hanya ingin berubah saat insert saja
         pass
+
+@receiver(post_save, sender=Akun)
+def membuat_kebun_disematkan(sender, instance, created, **kwargs):
+    if created:
+        KebunDisematkan.objects.create(id_akun=instance)
